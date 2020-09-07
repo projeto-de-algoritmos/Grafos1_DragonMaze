77f2789ec5acb90be9b4eba6c627ca4d122ed094
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-09-06 21:32:00 -0300

    Add dragon using BFS to search player
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

--- src/sketch.js
+++ src/sketch.js
@@ -1,5 +1,7 @@
 const LASTPHASE = 50;
 const NEXTPHASE = -10;
+const TIMETOUPDATEDRAGONPATH = 5;
+const TIMETOMOVEDRAGON = 3;
 let w = 100;
 let columns, rows;
 let stack = [];
@@ -10,6 +12,11 @@ let player;
 let end;
 let gameFinished = false;
 let img;
+let dragonImg;
+let dragonPath;
+let countMoveDragonTime = 3;
+let countUpdateDragonPath = 0;
+let gameOver = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -22,6 +29,9 @@ function setup() {
   player = board[0][0];
   end = board[columns - 1][rows - 1];
   end.final = true;
+  dragon = board[floor(random() * columns)][floor(random() * rows)];
+  //dragon = player;
+  setInterval(timeIt, 1000);
 }
 
 
@@ -38,16 +48,33 @@ function reset() {
   current = '';
   player = '';
   end = '';
+  dragonPath = '';
+  countMoveDragonTime = 3;
+  countUpdateDragonPath = 0;
   setup()
 }
-function preload(){
+function preload() {
   img = loadImage('assets/character.gif');
+  dragonImg = loadImage('assets/dragon.gif');
 }
 
 function draw() {
   background(54, 54, 54);
   if (gameFinished) {
-    text('CONGRATULATIONS', 10, 30);
+    textAlign(CENTER, CENTER);
+    textSize(width * 0.02);
+    fill(255);
+    text('Congratulatios', width / 2, height / 2);
+  } else if (dragon.id === player.id) {
+    textAlign(CENTER, CENTER);
+    textSize(width * 0.03);
+    fill(255);
+    text('Game over', width / 2, height/4);
+    textAlign(CENTER, CENTER);
+    textSize(width * 0.01);
+    fill(255);
+    text('Press Enter to continue...', width / 2, height * 0.9);
+    gameOver = true;
   } else {
     for (x = 0; x < board.length; x++) {
       for (y = 0; y < board[x].length; y++) {
@@ -70,24 +97,27 @@ function draw() {
   } else if (stack.length > 0) {
     current = stack.pop();
 
-    if (current.x === 0 && current.y === 0) {
+    if (current.id === "00") {
       mazeFinished = true;
     }
   }
 
   if (mazeFinished) {
     player.player = true;
-    player.img = img;
-    
+    dragon.dragon = true;
+    if (countUpdateDragonPath === 0)
+      defineDragonRoute();
   }
 
-  if (player.x === end.x && player.y === end.y) {
+  if (player.id === end.id) {
     reset();
+
   }
 }
 
 function keyPressed() {
-  if (!mazeFinished) {
+
+  if (!mazeFinished || gameOver) {
     return null;
   }
 
@@ -144,5 +174,59 @@ function removeWall(current, next) {
   } else if (y === -1) {
     current.walls.bottom = false;
     next.walls.top = false;
+  }
+}
+
+function defineDragonRoute() {
+  let paths = [];
+  let path = [];
+  path.push(dragon);
+  paths.push([...path]);
+
+  for (x = 0; x < board.length; x++) {
+    for (y = 0; y < board[x].length; y++) {
+      board[x][y].dragonVisited = false;
+    }
+  }
+
+  while (paths.length > 0) {
+    path = paths.shift();
+    let last = path[path.length - 1]
+    if (last.id === player.id) {
+      break;
+    }
+
+    let neighbors = last.allNeighbors();
+
+    for (let i = 0; i < neighbors.length; i++) {
+      if (!neighbors[i].dragonVisited) {
+        neighbors[i].dragonVisited = true;
+        let newPath = [...path];
+        newPath.push(neighbors[i]);
+        paths.push([...newPath]);
+      }
+    }
+  }
+
+  dragonPath = path;
+  dragonPath.shift();
+}
+
+function timeIt() {
+  if (mazeFinished && !gameOver) {
+    if (countMoveDragonTime > 0) {
+      countMoveDragonTime--;
+    } else {
+      if (dragonPath.length > 0) {
+        dragon.dragon = false;
+        dragon = dragonPath.shift();
+        countMoveDragonTime = TIMETOMOVEDRAGON;
+      }
+    }
+    if (countUpdateDragonPath > 0) {
+      countUpdateDragonPath--;
+    } else {
+      countUpdateDragonPath = TIMETOUPDATEDRAGONPATH;
+    }
   }
 }
